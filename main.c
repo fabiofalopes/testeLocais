9493c6da6b1e3c7542a9c16d05a75fc92088c88d
fabio <fabiofalopes@gmail.com>
2023-05-01 19:48:25 +0100

    square

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
+
+void drawFrame(int width, int height, char frameChar) {
+    // Draw top border
+    for (int i = 0; i < width; i++) {
+        printf("%c", frameChar);
+    }
+    printf("\n");
+
+    // Draw sides
+    for (int i = 1; i < height - 1; i++) {
+        printf("%c", frameChar);
+        for (int j = 1; j < width - 1; j++) {
+            printf(" ");
+        }
+        printf("%c\n", frameChar);
+    }
+
+    // Draw bottom border
+    for (int i = 0; i < width; i++) {
+        printf("%c", frameChar);
+    }
+}
+
 int main(int argc, char *argv[]) {
+
+
+
   printf("Hello world!\n");
+  drawFrame(10,10,'*');
+
 
   return 0;
 }
